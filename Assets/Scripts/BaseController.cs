90cc2d96a91a9ad25d11b398094295a0265434e4
Kim199610 <kk2247401@naver.com>
2025-02-23 11:23:59 +0900

    플레이어 움직임 구현

--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseController : MonoBehaviour
+public abstract class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected Rigidbody2D characterRigidbody;
+    [SerializeField] protected SpriteRenderer characterSprite;
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    protected virtual void Awake()
+    {
+        characterRigidbody = GetComponent<Rigidbody2D>();
+        characterSprite =GetComponentInChildren<SpriteRenderer>();
+
+    }
+    protected virtual void Start()
     {
         
+    }
+    protected virtual void Update()
+    {
+        HandleAction();
+        Rotate(LookDirection);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void FixedUpdate()
     {
-        
+        MoveCharacter(movementDirection);
     }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+    protected virtual void MoveCharacter(Vector2 MovementDirection)
+    {
+        movementDirection = MovementDirection * 5/*player speed State*/;
+
+        characterRigidbody.velocity = movementDirection;
+    }
+
+    protected virtual void Rotate(Vector2 LookDirection)
+    {
+        bool isLeft = Mathf.Abs(Mathf.Atan2(LookDirection.y, LookDirection.x)) > (Mathf.PI / 2);
+        
+        characterSprite.flipX = isLeft;
+    }
+
 }
