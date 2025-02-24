c37ca3cde6e1ee03d0dd877bd358e0a0fe2bd952
hchee99 <hchee99@naver.com>
2025-02-24 10:47:44 +0900

    [new]

---
+++ Assets/Scripts/Skill/BaseSkill.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseSkill : MonoBehaviour
+{
+    public string skillName;    // 스킬 이름
+    public float coolDown;      // 쿨다운 시간
+    public float duration;      // 스킬 효과 지속 시간
+    public int level = 1;       // 스킬 레벨 (기본 1)
+
+    // 스킬 발동 시 호출되는 함수 (플레이어 오브젝트를 인자로 받음)
+    public abstract void Activate(GameObject user);
+
+    // 스킬 업그레이드 (레벨 상승)
+    public virtual void UpgradeSkill()
+    {
+        level++;
+    }
+}
