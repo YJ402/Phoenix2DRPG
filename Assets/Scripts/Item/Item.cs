ac3356e60dc357dd1385ee019b010fe9597cec1d
snaggytoothy <snaggytoothy@gmail.com>
2025-02-27 16:07:00 +0900

    [fix] 아이템 제거
    아이템 제거 시 itemManager 리스트에서 해당 아이템이 없어지도록 추가

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     private ResourceController resourceController;
+    public ItemManager itemManager;
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == "Player")
         {
             resourceController.ChangeHealth(20f);
         }
+        itemManager.spawnedItems.Remove(this.gameObject);
+        Debug.Log($"{this.gameObject} removed");
         Destroy(this);
     }
 }
