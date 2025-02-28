f7257f39a6c5a9e7bb1759d1a70f3677916710d6
Kim199610 <kk2247401@naver.com>
2025-02-28 10:52:44 +0900

    오류수정

--- Assets/Scripts/Enemy/MeleeEnemyController.cs
+++ Assets/Scripts/Enemy/MeleeEnemyController.cs
@@ -9,7 +9,7 @@ public class MeleeEnemyController : EnemyController
     public override void CheckHit()
     {
         //RaycastHit2D hit = Physics2D.BoxCast(hitBox.position, hitBox.lossyScale, 0, lookDirection.normalized, 0, targetLayerMask);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, lookDirection, statHandler.AttackRange + 500.2f, (1 << targetLayerMask));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, lookDirection, statHandler.AttackRange + 0.2f, (1 << targetLayerMask));
         Debug.DrawRay(transform.position, lookDirection* (statHandler.AttackRange + 0.2f), Color.red);
         if (hit.collider != null)
             Debug.Log("근접 공격 성공");
